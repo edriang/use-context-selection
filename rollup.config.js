627762a71e398ef737d9c3095ff7b05c68f500a3
Adrián Gallardo <edriang@gmail.com>
2020-04-21 22:02:04 -0500

    feat: exports typescript declaration files

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript3';
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
